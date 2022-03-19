a7a145c5845e3534d9cc5f9ca2a0b6c26dcf6515
eliteKebob <aykut.ozmen3@gmail.com>
2022-03-19 20:51:39 +0300

    changes v2

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import styles from "../styles/Landing.module.css";
+import AgentsImage from "../assets/img/full.png";
 
 const Error = () => {
-  return <div>Error 404!</div>;
+  return (
+    <div className={styles.landingBody}>
+      <h1>HATA!</h1>
+      <p>Talep ettiğiniz sayfaya ulaşamadık!</p>
+      <img src={AgentsImage} alt="" />
+    </div>
+  );
 };
 
 export default Error;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import styles from "../styles/Landing.module.css";
+import AgentsImage from "../assets/img/full.png";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className={styles.landingBody}>
+      <h1>Valopedia</h1>
+      <p>
+        Valopedia'ya hoşgeldiniz! Navigasyon menüsünden merak ettiklerinize
+        ulaşabilirsiniz!
+      </p>
+      <img src={AgentsImage} alt="" />
+    </div>
+  );
 };
 
 export default Landing;
